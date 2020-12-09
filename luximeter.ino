5dedfe8ce2bf7644561645d28bf9c3de6d7baf1c
Danilo Santana <daniloxc@msn.com>
2020-12-09 15:57:56 -0300

    Desenvolvimento da funcionalidade de captura de coordenadas do GPS

--- luximeter.ino
+++ luximeter.ino
@@ -1,21 +1,61 @@
 #include <Wire.h> 
 #include <BH1750.h> 
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
  
+TinyGPS gps;
+ 
+#define GPS_RX 10
+#define GPS_TX 11
+#define GPS_Serial_Baud 9600
+
+// The serial connection to the GPS device
+SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
+
+//Lightmeter
 BH1750 lightMeter; 
  
 void setup(){
   Serial.begin(9600);
   Wire.begin();
   lightMeter.begin();
+  gpsSerial.begin(GPS_Serial_Baud);
 }
  
 void loop() {
   
-  Serial.print(getLux(lightMeter)); 
-  delay(1000); 
+  Serial.println(getData());
 }
 
 
-float getLux(BH1750 lightMeter){
+float getLux(){
   return lightMeter.readLightLevel();
+}
+
+String getData(){
+  bool newData = false;
+  unsigned long chars;
+  
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (gpsSerial.available())
+    {
+      char c = gpsSerial.read();
+       
+      if (gps.encode(c))
+        newData = true;
+    }
+  }
+  if (newData)
+  {
+    float flat, flon;
+    String lat, lon, lux;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    lux = String(getLux());
+    
+    lat = String(flat,6);
+    lon = String(flon,6);
+    return lat+","+lon+","+lux;
+  }
 }
