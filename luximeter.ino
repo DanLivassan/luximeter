b0d3bb089876f450283717b8ce72d4cbc405c801
Danilo Santana <daniloxc@msn.com>
2020-12-09 15:36:03 -0300

    Função para ler o luximetro BH1750 desenvolvida

--- luximeter.ino
+++ luximeter.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
-
+#include <Wire.h> 
+#include <BH1750.h> 
+ 
+BH1750 lightMeter; 
+ 
+void setup(){
+  Serial.begin(9600);
+  Wire.begin();
+  lightMeter.begin();
+}
+ 
+void loop() {
+  
+  Serial.print(getLux(lightMeter)); 
+  delay(1000); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+float getLux(BH1750 lightMeter){
+  return lightMeter.readLightLevel();
 }
